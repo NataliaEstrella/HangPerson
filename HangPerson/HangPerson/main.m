4eee078636bd8f562878ce5a4775d86a1e86b27a
Natalia Estrella <natalia.c.estrella@gmail.com>
2015-06-19 20:08:34 -0400

    This is my final hangPersonProject

--- HangPerson/HangPerson/main.m
+++ HangPerson/HangPerson/main.m
@@ -11,10 +11,35 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        // code goes here...
+        char hangWord[13] = "pumpernickle";
+        char wordBlank[13] = "____________";
         
-        printf("hi my name is mike");
-
+        char letterInput;
+        int numTry;
+        int maxTry = 15;
+        
+        
+        for (numTry = 0; numTry < 15; numTry++) {
+            
+            printf("Guess a letter? ");
+            scanf(" %c", &letterInput);
+            
+            for (int i = 0 ; i < 12; i++) {
+                if (letterInput == hangWord[i]) {
+                    wordBlank[i] = letterInput;
+                }
+            }
+            
+            printf("%s\n", wordBlank);
+            if (strcmp(hangWord, wordBlank) == 0) {
+                printf("You Won!");
+                break;
+            }
+        }
+        if (numTry >= maxTry) {
+            printf("You have failed at life");
+        }
+        
     }
     return 0;
 }
